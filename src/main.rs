c8b47b866bf9ae522fb30a02aebbc66b30e891bd
Ethan Hetrick <rqu4@cdc.gov>
2026-08-03 17:15:21 -0400

    reject incompatible bytes

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,71 @@ fn main() -> io::Result<()> {
 
     for path in env::args().skip(1) {
 
-            // Check if file exists
+            // check: does exist
             assert!(fs::exists(&path).unwrap(), "\n\tERROR: Unable to access file {path}\n");
 
-            // Make sure file is not a dir
+            // check: is not dir
             let metadata = fs::metadata(&path)?;
             assert!(!metadata.is_dir(), "\nERROR: {path} is a directory\n");
 
-            // Make sure file is not empty
+            // check: is not empty
             assert!(metadata.len() > 0, "\nERROR: {path} is empty\n");
 
             // Load file
             let contents: Vec<u8> = fs::read(&path)?;
 
-            // Check for UTF BOM
+            // check: does not have UTF BOM
             assert_ne!(&contents[0..3], [0xEF, 0xBB, 0xBF], "\n\nERROR: {path} contains UTF BOM. Remove it using:\n\n\t\tdos2unix --remove-bom {path}\n");
 
-            // Check for gzip magic bytes
+            // check: has gzip magic bytes
+            let mut gzip: bool = false;
             if &contents[0..2] == [0x1F, 0x8B] {
                 println!("\n{path} is gzip-compressed\n");
+                gzip = true;
+            }
+
+            // Offending ASCII bytes
+            // The below are mostly control characters with exceptions like NUL and CR
+            let offenders: Vec<u8> = vec![
+                0x00, // NUL
+                0x01, // SOH
+                0x02, // STX
+                0x03, // ETX
+                0x04, // EOT
+                0x05, // ENQ
+                0x06, // ACK
+                0x07, // BEL
+                0x08, // BS
+                0x0B, // VT
+                0x0C, // FF
+                0x0D, // CR
+                0x0E, // SO
+                0x0F, // SI
+                0x10, // DLE
+                0x11, // DC1
+                0x12, // DC2
+                0x13, // DC3
+                0x14, // DC4
+                0x15, // NAK
+                0x16, // SYN
+                0x17, // ETB
+                0x18, // CAN
+                0x19, // EM
+                0x1A, // SUB
+                0x1B, // ESC
+                0x1C, // FS
+                0x1D, // GS
+                0x1E, // RS
+                0x1F, // US
+                0x7F, // DEL
+            ];
+
+            for byte in contents.iter() {
+                // check: all bytes are ASCII
+                if !gzip { assert!(byte.is_ascii(), "\nERROR: {path} contains non-ASCII bytes.\n"); }
+
+                // check: void of offending ASCII bytes
+                if !gzip { assert!(!offenders.contains(byte)); }
             }
 
             // FASTA header check
