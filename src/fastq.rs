9cfc10f8d0b4647a16ff9dfc101cee88c5a20492
Ethan Hetrick <rqu4@cdc.gov>
2026-08-05 15:46:26 -0400

    feat: add file extension checks

--- src/fastq.rs
+++ src/fastq.rs
@@ -1,14 +1,19 @@
+const VALID_FASTQ_EXTENSIONS: [&str; 2] = [".fastq", ".fq"];
+
 #[derive(Debug)]
 pub struct Fastq {
+    pub valid_extension: bool,
     pub valid_start: bool,
     pub four_line_entries: bool,
 }
 
 impl Fastq {
-    pub fn new(contents: &[u8], size: &usize) -> Self {
+    pub fn new(contents: &[u8], line_count: &usize, path: &String) -> Self {
         Fastq {
+            valid_extension: VALID_FASTQ_EXTENSIONS
+            .iter().any(|&ext| path.ends_with(ext) || path.ends_with(&format!("{}.gz", ext))),
             valid_start: contents.starts_with(&[0x40]),
-            four_line_entries: (size % 4 == 0),
+            four_line_entries: (line_count % 4 == 0),
         }
     }
 }
