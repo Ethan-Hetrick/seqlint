663cda2c4462f1c9d5d8b1ed6accea8db18d4df9
Ethan Hetrick <rqu4@cdc.gov>
2026-08-09 03:22:48 -0400

    style: cargo fmt

--- src/scan.rs
+++ src/scan.rs
@@ -125,7 +125,7 @@ pub fn bytewise_checks(contents: &[u8], pipeline: &str) -> ByteWiseCheck {
 
             if pipeline == "fastq" {
                 if (*byte == b'@' && line_count == 0)
-                    || (i >= 2 && *byte == b'@' && contents[i - 2] == b'\n' && line_count %4 == 0)
+                    || (i >= 2 && *byte == b'@' && contents[i - 2] == b'\n' && line_count % 4 == 0)
                 {
                     record_count += 1;
                     if sequence_length != quality_length {
@@ -145,7 +145,8 @@ pub fn bytewise_checks(contents: &[u8], pipeline: &str) -> ByteWiseCheck {
                         }
                         fastq_record.bad_sequence = true;
                     }
-                } else if (line_count + 1) % 4 == 0 { // Quality scores line
+                } else if (line_count + 1) % 4 == 0 {
+                    // Quality scores line
                     quality_length += 1;
                 }
             }
@@ -212,7 +213,7 @@ pub fn bytewise_checks(contents: &[u8], pipeline: &str) -> ByteWiseCheck {
     }
 
     if sequence_length != quality_length {
-            println!("- Sequence and quality line lengths do not match");
+        println!("- Sequence and quality line lengths do not match");
     }
 
     if record_count == 0 {
