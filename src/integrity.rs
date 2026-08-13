4aebbc349af2b8fbb7fd388b6994994d8640a49b
Ethan Hetrick <rqu4@cdc.gov>
2026-08-13 00:38:40 -0400

    feat: add file recursion option

--- src/integrity.rs
+++ src/integrity.rs
@@ -1,4 +1,7 @@
 use std::fs;
+use std::collections::HashSet;
+use walkdir::WalkDir;
+use std::path::PathBuf;
 
 enum FailureStates {
     NotReadable,
@@ -14,6 +17,71 @@ impl FailureStates {
             FailureStates::TooSmall => "< 3 bytes",
         }
     }
+}
+
+pub fn generate_canonical_path_set(path_buffers: Vec<PathBuf>, recursive: bool, max_depth: Option<usize>, follow: bool) -> HashSet<String> {
+    let mut file_set: HashSet<String> = HashSet::new();
+
+    for path_buf in &path_buffers {
+        if path_buf.to_str().is_none() {
+            eprintln!(
+                "\nERROR: path '{}' contains non-UTF-8 characters, skipping..",
+                path_buf.display()
+            );
+            continue;
+        }
+
+        if recursive {
+            let walker = if max_depth.is_some() {
+                WalkDir::new(path_buf)
+                    .max_depth(max_depth.unwrap())
+                    .follow_links(follow)
+                    .sort_by_file_name()
+            } else {
+                WalkDir::new(path_buf)
+                    .follow_links(follow)
+                    .sort_by_file_name()
+            };
+
+            for entry in walker {
+                let entry = match entry {
+                    Ok(e) => e,
+                    Err(err) => {
+                        eprintln!("\nWARNING: skipping unreadable entry: {err}");
+                        continue;
+                    }
+                };
+
+                let canonical_path = match fs::canonicalize(entry.path()) {
+                    Ok(p) => p,
+                    Err(e) => {
+                        eprintln!("\nERROR: Path: '{}' {e}, skipping..", entry.path().display());
+                        continue;
+                    }
+                };
+
+            file_set.insert(canonical_path.to_string_lossy().into_owned());
+            }
+            
+        } else {
+            let canonical_path = match fs::canonicalize(&path_buf) {
+                    Ok(p) => p,
+                    Err(e) => {
+                        eprintln!("\nERROR: Path: '{}' {e}, skipping..", path_buf.display());
+                        continue;
+                    }
+            };
+
+            if !canonical_path.is_file() {
+                        eprintln!("Skipping directory (use -R to recurse): {}", canonical_path.display());
+                        continue;
+            }
+
+            file_set.insert(canonical_path.to_string_lossy().into_owned());
+        }
+}
+
+    file_set
 }
 
 pub fn integrity_checks(path: &String) -> Result<(), &'static str> {
