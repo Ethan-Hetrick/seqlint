cb1794cb2d0b87822a0ccf09fa81873e1410ce33
Ethan Hetrick <rqu4@cdc.gov>
2026-08-21 00:46:15 -0400

    feat: print bam header if found

--- src/bam.rs
+++ src/bam.rs
@@ -1,25 +1,59 @@
-const BAM_HEADER: &[u8; 4] = b"BAM\x01";
+const BAM_MAGIC: &[u8; 4] = b"BAM\x01";
 
 use seqlint::{info,log};
 
 // BAM specific file checks
 pub struct Bam {
-    bam_header: bool
+    bam_magic: bool,
+    bam_header: Option<String>,
 }
 
 impl Bam {
     pub fn new(contents: &Vec<u8>) -> Bam {
+        let bam_magic = contents.starts_with(BAM_MAGIC);
+
+        let bam_header = if bam_magic {
+            Bam::bam_header(&contents[5..])
+        } else {
+            None
+        };
+
         let bam = Bam {
-            bam_header: contents.starts_with(BAM_HEADER)
+            bam_magic: bam_magic,
+            bam_header: bam_header,
         };
 
         bam
     }
 
+    fn bam_header(contents: &[u8]) -> Option<String> {
+        let mut bytes: Vec<u8> = Vec::new();
+        let mut i = 0;
+
+        for byte in contents.iter() {
+            if *byte == b'\n' && contents.get(i + 1) != Some(&b'@') {
+                return Some(String::from_utf8_lossy(&bytes)
+                    .to_string()
+                    .split_whitespace().collect::<Vec<_>>().join(" "));
+            } else {
+                bytes.push(*byte);
+            }
+
+            i += 1;
+        }
+
+        None
+    }
+
     pub fn report(&self) {
         log!("== BAM checks ==");
-        if self.bam_header == true {
-            info!("BAM header present")
+        if self.bam_magic == true {
+            info!("BAM magic string present")
+        }
+
+        match &self.bam_header {
+            Some(header) => info!("BAM header: {header}"),
+            _ => info!("No BAM header returned")
         }
     }
 }
